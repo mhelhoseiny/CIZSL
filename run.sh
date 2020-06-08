283086c5cefd42c5b428c776d990f9c00921c261
Mohamed Elfeki <m.elfeki11@gmail.com>
2020-06-08 06:46:12 -0700

    Run File
    To reproduce reported results

---
+++ run.sh
@@ -0,0 +1,4 @@
+python train_CIZSL.py --dataset 'CUB' --splitmode 'hard' --creativity_weight 0.1
+python train_CIZSL.py --dataset 'NAB' --splitmode 'hard' --creativity_weight 0.1
+python train_CIZSL.py --dataset 'NAB' --splitmode 'easy' --creativity_weight 1
+python train_CIZSL.py --dataset 'CUB' --splitmode 'easy' --creativity_weight 0.0001
